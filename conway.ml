21fa407651fa337ddec96f5e549e43c88e3c5ff8
xjpa <xjpa@proton.me>
2023-09-24 03:29:43 +0800

    change grid init to random live and dead cells

--- conway.ml
+++ conway.ml
@@ -1,64 +1,58 @@
 type cell = Dead | Alive
-type grid = cell array array
+type grid = cell list list
 
 let get_cell grid x y =
-  if x < 0 || x >= Array.length grid || y < 0 || y >= Array.length grid.(0) then
-    Dead
-  else
-    grid.(x).(y)
+  try
+    match List.nth (List.nth grid x) y with
+    | Alive -> Alive
+    | Dead -> Dead
+  with _ -> Dead
 
 let count_alive_neighbors grid x y =
-  let dx = [|-1; -1; -1;  0; 0;  1; 1; 1|] in
-  let dy = [|-1;  0;  1; -1; 1; -1; 0; 1|] in
-  let count = ref 0 in
-  for i = 0 to 7 do
-    match get_cell grid (x + dx.(i)) (y + dy.(i)) with
-    | Alive -> incr count
-    | Dead -> ()
-  done;
-  !count
+  let directions = [(-1, -1); (-1, 0); (-1, 1); (0, -1); (0, 1); (1, -1); (1, 0); (1, 1)] in
+  List.fold_left (fun acc (dx, dy) -> 
+    match get_cell grid (x + dx) (y + dy) with
+    | Alive -> acc + 1
+    | Dead -> acc
+  ) 0 directions
 
 let compute_next grid =
-  let new_grid = Array.make_matrix (Array.length grid) (Array.length grid.(0)) Dead in
-  for x = 0 to Array.length grid - 1 do
-    for y = 0 to Array.length grid.(0) - 1 do
-      match grid.(x).(y), count_alive_neighbors grid x y with
-      | Alive, 2
-      | Alive, 3
-      | Dead,  3 -> new_grid.(x).(y) <- Alive
-      | _ -> new_grid.(x).(y) <- Dead
-    done
-  done;
-  new_grid
+  List.mapi (fun x row ->
+    List.mapi (fun y cell ->
+      match cell, count_alive_neighbors grid x y with
+      | Alive, 2 | Alive, 3 | Dead, 3 -> Alive
+      | _ -> Dead
+    ) row
+  ) grid
 
 let display_grid grid =
-  Array.iter (fun row ->
-      Array.iter (function
+  List.iter (fun row ->
+      List.iter (function
           | Dead -> print_char '.'
           | Alive -> print_char '#')
         row;
       print_newline ())
     grid
 
+let set_random_alive grid =
+  List.mapi (fun i row ->
+    List.mapi (fun j _ ->
+      if Random.bool () then Alive else Dead
+    ) row
+  ) grid
+
 let main () =
-  let grid = 
-    Array.make_matrix 10 10 Dead in
-
+  Random.self_init ();
+  let empty_row = List.init 20 (fun _ -> Dead) in
+  let grid_ref = ref (List.init 20 (fun _ -> empty_row)) in
   
-  grid.(1).(2) <- Alive;
-  grid.(2).(3) <- Alive;
-  grid.(3).(1) <- Alive;
-  grid.(3).(2) <- Alive;
-  grid.(3).(3) <- Alive;
+  grid_ref := set_random_alive !grid_ref;
 
-  let generations = 10 in
+  let generations = 20 in
   for _ = 1 to generations do
-    display_grid grid;
+    display_grid !grid_ref;
     print_newline ();
-    let new_grid = compute_next grid in
-    Array.blit new_grid 0 grid 0 (Array.length new_grid)
+    grid_ref := compute_next !grid_ref;
   done
 
 let () = main ()
-        
-
